068d15e3c814fc7533575acbf825a19c0d375518
EsmaelChande <esarcha06@gmail.com>
2024-05-21 09:39:41 +0200

    intersacao de dados na H2 commit

---
+++ github.word.api/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_paises(id, nome, regiao, area)VALUES(1,'Mocambique','Africa',4500);
+INSERT INTO tb_paises(id, nome, regiao, area)VALUES(2,'Portugal','Europa',2500);
+INSERT INTO tb_paises(id,nome, regiao, area)VALUES(3,'China','Asia',4000);
+INSERT INTO tb_paises(id, nome, regiao, area)VALUES(4,'Brazil','America Sul',500);
+INSERT INTO tb_paises(id,nome, regiao, area)VALUES(5,'America','America',4500);
--- github.word.api/src/main/resources/query.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tb_paises(nome, regiao, area)values('Mocambique','Africa',4500);
-INSERT INTO tb_Paises(nome, regiao, area)values('Portugal','Europa',2500);
-INSERT INTO tb_Paises(nome, regiao, area)values('China','Asia',4000);
-INSERT INTO tb_Paises(nome, regiao, area)values('Brazil','America Sul',500);
-INSERT INTO tb_Paises(nome, regiao, area)values('America','America',4500);
