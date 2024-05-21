1ad83a7d9834948d28ec5fafd0988de08baa933d
EsmaelChande <esarcha06@gmail.com>
2024-05-21 10:21:53 +0200

    4 envio commit

--- github.word.api/src/main/java/github/word/github/word/api/controller/ControllerPais.java
+++ github.word.api/src/main/java/github/word/github/word/api/controller/ControllerPais.java
@@ -1,16 +1,53 @@
 package github.word.github.word.api.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import github.word.github.word.api.entities.Pais;
+import github.word.github.word.api.repository.RepositoryPais;
 
 @RestController
 @RequestMapping(value="/pais")
 public class ControllerPais {
 	
-	@GetMapping(path="/status")
+	@Autowired
+	private RepositoryPais repositorio;
+	
+	
+	@GetMapping(path = "/status")
 	public String IsOnline() {
 		return "APIOnline";
 	}
 	
+	@GetMapping
+	public List<Pais> findAll() {
+		List<Pais> paises;
+		if (repositorio.findAll() != null) {
+			paises = repositorio.findAll();
+			return paises;
+		} else {
 
+			return null;
+		}
+
+	}
+	
+	@GetMapping(value="lista/{id}")
+public Pais findById(@PathVariable Integer id) {
+	Pais pais;
+	if(repositorio.existsById(id)) {
+		pais= repositorio.findById(id).get();
+		return pais;
+	}else {
+		return null;
+	}
+	
+	
+}
+	
+	
 }
---
+++ github.word.api/src/main/java/github/word/github/word/api/repository/RepositoryPais.java
@@ -0,0 +1,8 @@
+package github.word.github.word.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import github.word.github.word.api.entities.Pais;
+
+public interface RepositoryPais extends JpaRepository<Pais, Integer>{
+
+}
